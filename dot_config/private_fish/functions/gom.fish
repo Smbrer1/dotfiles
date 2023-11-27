225c41e48e42cc9ea9bdd66ec5720d66c06bf508
Artem Sokolov <avsokolov@mtt.ru>
2023-11-27 20:51:45 +0300

    added some functions to fish and little edit in i3

---
+++ dot_config/private_fish/functions/gom.fish
@@ -0,0 +1,4 @@
+function gom --wraps=go1.19 --description 'alias gom go1.19'
+  go1.19 $argv
+        
+end
---
+++ dot_config/private_fish/functions/lvim.fish
@@ -0,0 +1,4 @@
+function lvim --wraps='env NVIM_APPNAME=LazyNvim nvim' --description 'alias lvim env NVIM_APPNAME=LazyNvim nvim'
+  env NVIM_APPNAME=LazyNvim nvim $argv
+        
+end
